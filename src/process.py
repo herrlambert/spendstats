2d6f28a456a339a09c87f0a8867051e96d921e07
Matt Lambert <herrlambert@gmail.com>
2025-09-20 13:30:31 -0700

    add outputfiles to gitignore

--- src/process.py
+++ src/process.py
@@ -16,6 +16,7 @@ def get_filedata(files, mypath):
         with open(join(mypath, file)) as file:
             file_info['file_lines'] = [line.rstrip() for line in file]
     
+        print(file.name)
         file_name = file.name[10:17]
         if file_name[-1:] == '-':
             file_info['file_name'] = file_name[:-1]
@@ -28,19 +29,19 @@ def process_lines(file_name, file_lines):
     new_lines = []
     for line in lines:
         line = line.split(',')
-        line[0] = file_name
-        if len(line) > 6:
-            description = line[2] + line[3]
-            line[2] = description
-            del line[3]
+        line = line[1:]
+        if len(line) > 5:
+            description = line[1] + line[2]
+            line[1] = description
+            del line[2]
         
         # remove double-quotes surrounding description string
-        line[2] = line[2][1:-1]
+        line[1] = line[1][1:-1]
         
         # calculate total and assign to position 3
-        total = line[3] + line[4]
-        line[3] = total
-        del line[4]
+        total = line[2] + line[3]
+        line[2] = total
+        del line[3]
         
         new_lines.append(line)
         
@@ -67,7 +68,7 @@ def categorize_lines(lines):
     return categorized_lines
     
 def get_updated_data(file_data):
-    updated_data = [['statement', 'transdate', 'description', 'amount', 'who', 'category', 'subcategory']]
+    updated_data = [['transdate', 'description', 'amount', 'who', 'category', 'subcategory']]
     for item in file_data:
         lines = process_lines(item['file_name'], item['file_lines'])
         categorized_lines = categorize_lines(lines)
@@ -79,6 +80,6 @@ files = get_files(mypath)
 file_data = get_filedata(files, mypath)
 updated_data = get_updated_data(file_data)
 
-with open("../datafiles/all_transactions.csv", "w+") as my_csv:
+with open("../outputfiles/all_transactions.csv", "w+") as my_csv:
     csv_writer = csv.writer(my_csv, delimiter=',')
     csv_writer.writerows(updated_data)
